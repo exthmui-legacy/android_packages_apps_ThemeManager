783d3f57fed366c04afe29bb1f7cc029ab88f2a3
cjybyjk <cjybyjk@gmail.com>
2020-05-15 13:54:34 +0800

    ThemeManager: add privapp permissions xml
    Signed-off-by: cjybyjk <cjybyjk@gmail.com>

--- Android.mk
+++ Android.mk
@@ -22,7 +22,17 @@ LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_REQUIRED_MODULES := FakeFontsOverlay
 
+LOCAL_REQUIRED_MODULES += privapp_whitelist_org.exthmui.theme.xml
+
 include $(BUILD_PACKAGE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := privapp_whitelist_org.exthmui.theme.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := ThemeManagerStudio
