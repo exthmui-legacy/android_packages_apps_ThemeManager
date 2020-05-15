2acdde9697bf0bce6ff7132ff46975df2f4e39f3
cjybyjk <cjybyjk@gmail.com>
2020-05-15 13:55:31 +0800

    ThemeManager: [tmp] new UI

--- src/org/exthmui/theme/ThemePickerActivity.java
+++ src/org/exthmui/theme/ThemePickerActivity.java
@@ -26,7 +26,6 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.UserHandle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -34,14 +33,12 @@ import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.RotateAnimation;
 import android.widget.AdapterView;
-import android.widget.Switch;
+import android.widget.GridView;
 
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceManager;
 
-import org.exthmui.theme.fragments.ThemePickerFragment;
-import org.exthmui.theme.interfaces.ThemePickerInterface;
+import org.exthmui.theme.adapters.CommonAdapter;
 import org.exthmui.theme.misc.Constants;
 import org.exthmui.theme.models.ThemeBase;
 import org.exthmui.theme.services.ThemeDataService;
@@ -52,39 +49,40 @@ import org.exthmui.theme.utils.PermissionUtil;
 
 import java.util.List;
 
-public class ThemePickerActivity extends FragmentActivity implements ThemePickerInterface {
+public class ThemePickerActivity extends AppCompatActivity {
 
     private static final String TAG = "ThemePickerActivity";
 
     private List<ThemeBase> mThemeList;
     private ThemeDataService.ThemeDataBinder mThemeDataBinder;
     private ThemeDataConn mThemeDataConn;
-    private ThemePickerFragment mFragment;
 
-    private SharedPreferences mPreferences;
     private View mRefreshIconView;
     private RotateAnimation mRefreshAnimation;
+
+    private ThemeAdapter mThemeAdapter;
+    private GridView mGridView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.theme_picker_activity);
+
         NotificationUtil.createNotificationChannel(this, Constants.CHANNEL_APPLY_STATUS, getString(R.string.channel_apply_status), NotificationUtil.IMPORTANCE_DEFAULT);
         PermissionUtil.verifyStoragePermission(this);
         PermissionUtil.verifyWriteSettingsPermission(this);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.container, ThemePickerFragment.newInstance())
-                    .commitNow();
-        }
-
-        mPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         mRefreshAnimation = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f,
                 Animation.RELATIVE_TO_SELF, 0.5f);
         mRefreshAnimation.setInterpolator(new LinearInterpolator());
         mRefreshAnimation.setDuration(1000);
+
+        mGridView = findViewById(R.id.themesGrid);
+        int cardWidth = getResources().getDimensionPixelOffset(R.dimen.theme_card_image_height) / 16 * 10;
+        mGridView.setNumColumns(getResources().getDisplayMetrics().widthPixels / cardWidth);
+        mGridView.setOnItemClickListener(this::onThemeItemClick);
+        mGridView.setOnItemLongClickListener(this::onThemeItemLongClick);
 
         Intent mThemeDataService = new Intent(this, ThemeDataService.class);
         mThemeDataConn = new ThemeDataConn();
@@ -96,7 +94,7 @@ public class ThemePickerActivity extends FragmentActivity implements ThemePicker
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+        getMenuInflater().inflate(R.menu.menu_picker, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -108,14 +106,15 @@ public class ThemePickerActivity extends FragmentActivity implements ThemePicker
                 new Thread(() -> {
                     updateThemeList();
                     runOnUiThread(() -> {
-                        mFragment.updateAdapter();
+                        mThemeAdapter.notifyDataSetChanged();
                         refreshAnimationStop();
                     });
                 }).start();
                 return true;
             }
             case R.id.action_preferences: {
-                showPreferencesDialog();
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
                 return true;
             }
         }
@@ -148,63 +147,19 @@ public class ThemePickerActivity extends FragmentActivity implements ThemePicker
         }
     }
 
-    @Override
-    public Drawable getThemeImage(String packageName) {
-        return mThemeDataBinder.getThemeImage(packageName);
-    }
-
-    @Override
-    public List<ThemeBase> getThemeList() {
-        return mThemeList;
-    }
-
-    @Override
     public void updateThemeList() {
         mThemeDataBinder.updateThemeList();
     }
 
-    @Override
     public void onThemeItemClick(AdapterView<?> parent, View view, int position, long id) {
         Intent intent = new Intent(this, ThemePreviewActivity.class);
         intent.putExtra("theme", mThemeList.get(position).getPackageName());
         startActivity(intent);
     }
 
-    @Override
     public boolean onThemeItemLongClick(AdapterView<?> parent, View view, int position, long id) {
         askUninstallTheme(position);
         return true;
-    }
-
-    @Override
-    public void onAttachFragment(Fragment fragment) {
-        mFragment = (ThemePickerFragment) fragment;
-    }
-
-    private void showPreferencesDialog() {
-        View view = LayoutInflater.from(this).inflate(R.layout.preferences_dialog, null);
-        Switch overlayUninstallFlag = view.findViewById(R.id.preferences_overlay_uninstall_flag);
-        Switch forcedCenterWallpaper = view.findViewById(R.id.preferences_forced_center_wallpaper);
-
-        overlayUninstallFlag.setChecked(mPreferences.getBoolean(Constants.PREFERENCES_OVERLAY_REMOVE_FLAG, false));
-        forcedCenterWallpaper.setChecked(mPreferences.getBoolean(Constants.PREFERENCES_FORCED_CENTER_WALLPAPER, false));
-
-        new AlertDialog.Builder(this)
-                .setTitle(R.string.menu_preferences)
-                .setView(view)
-                .setOnDismissListener(dialogInterface -> {
-                    mPreferences.edit()
-                            .putBoolean(Constants.PREFERENCES_OVERLAY_REMOVE_FLAG,
-                                    overlayUninstallFlag.isChecked())
-                            .putBoolean(Constants.PREFERENCES_FORCED_CENTER_WALLPAPER,
-                                    forcedCenterWallpaper.isChecked())
-                            .apply();
-                    new Thread(() -> {
-                        updateThemeList();
-                        runOnUiThread(() -> mFragment.updateAdapter());
-                    }).start();
-                })
-                .show();
     }
 
     private void askUninstallTheme(int index) {
@@ -220,7 +175,7 @@ public class ThemePickerActivity extends FragmentActivity implements ThemePicker
                         @Override
                         public void onSuccess(String packageName) {
                             mThemeList.remove(index);
-                            mFragment.updateAdapter();
+                            mThemeAdapter.notifyDataSetChanged();
                         }
 
                         @Override
@@ -241,12 +196,36 @@ public class ThemePickerActivity extends FragmentActivity implements ThemePicker
             new Thread(() -> {
                 updateThemeList();
                 mThemeList = mThemeDataBinder.getThemeBaseList();
-                runOnUiThread(() -> mFragment.updateView());
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mThemeAdapter = new ThemeAdapter(mThemeList, R.layout.theme_card);
+                        mGridView.setAdapter(mThemeAdapter);
+                    }
+                });
             }).start();
         }
 
         @Override
         public void onServiceDisconnected(ComponentName name) {
+        }
+    }
+
+
+    private class ThemeAdapter extends CommonAdapter<ThemeBase> {
+
+        public ThemeAdapter(List<ThemeBase> data, int layoutRes) {
+            super(data, layoutRes);
+        }
+
+        @Override
+        public void bindView(ViewHolder holder, ThemeBase obj) {
+            new Thread(() -> {
+                Drawable drawable =  mThemeDataBinder.getThemeImage(obj.getPackageName());
+                runOnUiThread(() -> holder.setImageResource(R.id.theme_card_image, drawable));
+            }).start();
+
+            holder.setText(R.id.theme_card_title, obj.getTitle());
         }
     }
 }
--- src/org/exthmui/theme/ThemePreviewActivity.java
+++ src/org/exthmui/theme/ThemePreviewActivity.java
@@ -27,21 +27,32 @@ import android.os.IBinder;
 import android.os.UserHandle;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
+
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import org.exthmui.theme.fragments.ThemeApplyingDialog;
-import org.exthmui.theme.fragments.ThemePreviewFragment;
-import org.exthmui.theme.interfaces.ThemePreviewInterface;
 import org.exthmui.theme.misc.Constants;
+import org.exthmui.theme.models.OverlayTarget;
 import org.exthmui.theme.models.ThemeItem;
 import org.exthmui.theme.services.ThemeDataService;
 import org.exthmui.theme.services.ThemeManageService;
 
 import java.util.List;
 
-public class ThemePreviewActivity extends FragmentActivity implements ThemePreviewInterface {
+public class ThemePreviewActivity extends AppCompatActivity {
 
     private static final String TAG = "ThemePreviewActivity";
 
@@ -52,11 +63,24 @@ public class ThemePreviewActivity extends FragmentActivity implements ThemePrevi
     private ThemeManageService.ThemeApplyStatusListener mThemeApplyStatusListener;
 
     private String mThemePackageName;
-    private ThemePreviewFragment mFragment;
     private ThemeApplyingDialog mApplyingDialog;
     private ThemeItem mThemeItem;
+    private Bundle themeTargetBundle;
 
     private SharedPreferences mPreferences;
+
+    private LinearLayout appLayout;
+    private LinearLayout previewLayout;
+    private LinearLayout previewPicLayout;
+    private LinearLayout soundLayout;
+    private LinearLayout wallpaperLayout;
+    private LinearLayout othersLayout;
+    private TextView tvTitle;
+    private TextView tvAuthor;
+    private ImageView imageBanner;
+    private FloatingActionButton btnApply;
+
+    private Menu actionMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,14 +90,44 @@ public class ThemePreviewActivity extends FragmentActivity implements ThemePrevi
         Intent intent = getIntent();
         mThemePackageName = intent.getStringExtra("theme");
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.container, ThemePreviewFragment.newInstance())
-                    .commitNow();
-            mApplyingDialog = new ThemeApplyingDialog();
+        mPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        appLayout = findViewById(R.id.app_container);
+        previewLayout = findViewById(R.id.layout_previews);
+        previewPicLayout = findViewById(R.id.layout_preview_pic);
+        soundLayout = findViewById(R.id.sound_container);
+        wallpaperLayout = findViewById(R.id.wallpaper_container);
+        othersLayout = findViewById(R.id.others_container);
+        tvTitle = findViewById(R.id.theme_title);
+        tvAuthor = findViewById(R.id.theme_author);
+        imageBanner = findViewById(R.id.banner_image);
+        btnApply = findViewById(R.id.apply_theme_button);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_back_arrow);
         }
 
-        mPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        if (savedInstanceState != null) themeTargetBundle = savedInstanceState.getBundle("target_bundle");
+        if (themeTargetBundle == null) themeTargetBundle =  new Bundle();
+
+        btnApply.setOnClickListener(v -> applyTheme());
+
+        AppBarLayout appBarLayout = findViewById(R.id.app_bar);
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int i) {
+                if (actionMenu == null) return;
+                if (i < -imageBanner.getHeight()) {
+                    actionMenu.findItem(R.id.action_apply).setVisible(true);
+                } else {
+                    actionMenu.findItem(R.id.action_apply).setVisible(false);
+                }
+            }
+        });
 
         Intent mThemeDataService = new Intent(this, ThemeDataService.class);
         mThemeDataConn = new ThemeDataConn();
@@ -86,11 +140,37 @@ public class ThemePreviewActivity extends FragmentActivity implements ThemePrevi
     @Override
     public void onAttachFragment(Fragment fragment) {
         super.onAttachFragment(fragment);
-        if (fragment instanceof ThemePreviewFragment) {
-            mFragment = (ThemePreviewFragment) fragment;
-        } else if (mApplyingDialog == null && fragment instanceof ThemeApplyingDialog) {
+        if (mApplyingDialog == null && fragment instanceof ThemeApplyingDialog) {
             mApplyingDialog = (ThemeApplyingDialog) fragment;
         }
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBundle("target_bundle", themeTargetBundle);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_preview, menu);
+        actionMenu = menu;
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case android.R.id.home:
+                finish();
+                break;
+            case R.id.action_apply:
+                applyTheme();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -105,30 +185,120 @@ public class ThemePreviewActivity extends FragmentActivity implements ThemePrevi
             } catch (Exception e) {
                 Log.e(TAG, "Failed to remove theme apply status listener!");
             }
-
             unbindService(mThemeManageConn);
         }
     }
 
-    @Override
-    public List<Drawable> getThemePreviewList(String packageName) {
-        return mThemeDataBinder.getThemePreviewList(packageName);
+
+    private void updateViewForTheme() {
+        tvTitle.setText(mThemeItem.getTitle());
+        tvAuthor.setText(mThemeItem.getAuthor());
+        imageBanner.setImageDrawable(mThemeDataBinder.getThemeBanner(mThemeItem.getPackageName()));
+
+        // wallpaper
+        if (mThemeItem.hasWallpaper()) {
+            addSwitch(wallpaperLayout, Constants.THEME_TARGET_WALLPAPER, R.string.background_wallpaper);
+        }
+        if (mThemeItem.hasLockScreen()) {
+            addSwitch(wallpaperLayout, Constants.THEME_TARGET_LOCKSCREEN, R.string.background_lockscreen);
+        }
+        if (!mThemeItem.hasWallpaper() && !mThemeItem.hasLockScreen()) {
+            wallpaperLayout.setVisibility(View.GONE);
+        }
+
+        // sound
+        if (mThemeItem.hasRingtone()) {
+            addSwitch(soundLayout, Constants.THEME_TARGET_RINGTONE, R.string.sound_ringtone);
+        }
+        if (mThemeItem.hasAlarmSound()) {
+            addSwitch(soundLayout, Constants.THEME_TARGET_ALARM, R.string.sound_alarm);
+        }
+        if (mThemeItem.hasNotificationSound()) {
+            addSwitch(soundLayout, Constants.THEME_TARGET_NOTIFICATION, R.string.sound_notification);
+        }
+        if (!mThemeItem.hasRingtone() && !mThemeItem.hasAlarmSound() && !mThemeItem.hasNotificationSound()) {
+            soundLayout.setVisibility(View.GONE);
+        }
+
+        // others
+        if (mThemeItem.hasBootanimation) {
+            addSwitch(othersLayout, Constants.THEME_TARGET_BOOTANIMATION, R.string.others_bootanimation);
+        }
+        if (mThemeItem.hasFonts) {
+            addSwitch(othersLayout, Constants.THEME_TARGET_FONTS, R.string.others_fonts);
+        }
+        if (!mThemeItem.hasFonts && !mThemeItem.hasBootanimation){
+            othersLayout.setVisibility(View.GONE);
+        }
+
+        // apps
+        if (mThemeItem.hasOverlays()) {
+            for (OverlayTarget overlayTarget : mThemeItem.getOverlayTargets()) {
+                addSwitch(appLayout, overlayTarget.getPackageName(), overlayTarget.getLabel(), overlayTarget.getSwitchable());
+            }
+        } else {
+            appLayout.setVisibility(View.GONE);
+        }
+
+        // previews
+        List<Drawable> previewList = mThemeDataBinder.getThemePreviewList(mThemeItem.getPackageName());
+        if (!previewList.isEmpty()) {
+            for (Drawable drawable : previewList) {
+                addPreview(drawable);
+            }
+        } else {
+            previewLayout.setVisibility(View.GONE);
+        }
+
     }
 
-    @Override
-    public Drawable getThemeBanner(String packageName) {
-        return mThemeDataBinder.getThemeBanner(packageName);
+    private void addSwitch(LinearLayout layout, final String id, int text) {
+        addSwitch(layout, id, getString(text), true);
     }
 
-    @Override
-    public void applyTheme(Bundle bundle) {
+    private void addSwitch(LinearLayout layout, final String id, String text, boolean enabled) {
+
+        Switch tmpSwitch = new Switch(this);
+
+        tmpSwitch.setText(text);
+        tmpSwitch.setChecked(themeTargetBundle.getBoolean(id, true));
+
+        tmpSwitch.setOnCheckedChangeListener((compoundButton, b) -> themeTargetBundle.putBoolean(id, b));
+
+        tmpSwitch.setEnabled(enabled);
+
+        layout.addView(tmpSwitch);
+    }
+
+    private void addPreview(Drawable drawable) {
+
+        int imageHeight = getResources().getDimensionPixelOffset(R.dimen.preview_image_height);
+
+        ImageView imageView = new ImageView(this);
+        imageView.setImageDrawable(drawable);
+
+        imageView.setMaxHeight(imageHeight);
+        imageView.setAdjustViewBounds(true);
+
+        imageView.setOnClickListener(v -> {
+            /*
+            imagePreviewViewer.setImageDrawable(imageView.getDrawable());
+            imagePreviewViewer.setVisibility(View.VISIBLE);
+            */
+        });
+
+        previewPicLayout.addView(imageView);
+
+    }
+
+    public void applyTheme() {
         if (mApplyingDialog == null) mApplyingDialog = new ThemeApplyingDialog();
         mApplyingDialog.show(getSupportFragmentManager(), TAG);
-        bundle.putBoolean(Constants.PREFERENCES_OVERLAY_REMOVE_FLAG, mPreferences.getBoolean(Constants.PREFERENCES_OVERLAY_REMOVE_FLAG, false));
-        bundle.putBoolean(Constants.PREFERENCES_FORCED_CENTER_WALLPAPER, mPreferences.getBoolean(Constants.PREFERENCES_FORCED_CENTER_WALLPAPER, false));
+        themeTargetBundle.putBoolean(Constants.PREFERENCES_OVERLAY_REMOVE_FLAG, mPreferences.getBoolean(Constants.PREFERENCES_OVERLAY_REMOVE_FLAG, false));
+        themeTargetBundle.putBoolean(Constants.PREFERENCES_FORCED_CENTER_WALLPAPER, mPreferences.getBoolean(Constants.PREFERENCES_FORCED_CENTER_WALLPAPER, false));
         new Thread(() -> {
             if (mThemeItem != null) {
-                mThemeManageBinder.applyTheme(mThemeItem, bundle);
+                mThemeManageBinder.applyTheme(mThemeItem, themeTargetBundle);
             }
         }).start();
     }
@@ -139,7 +309,7 @@ public class ThemePreviewActivity extends FragmentActivity implements ThemePrevi
             mThemeDataBinder = (ThemeDataService.ThemeDataBinder) iBinder;
             if (mThemeDataBinder.isThemePackage(mThemePackageName)) {
                 mThemeItem = mThemeDataBinder.getThemeItem(mThemePackageName);
-                mFragment.setThemeItem(mThemeItem);
+                updateViewForTheme();
             }
         }
 
@@ -153,7 +323,7 @@ public class ThemePreviewActivity extends FragmentActivity implements ThemePrevi
         public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
             mThemeManageBinder = (ThemeManageService.ThemeManageBinder) iBinder;
             mThemeApplyStatusListener = data -> {
-                if (mFragment == null || mApplyingDialog == null) return false;
+                if (mApplyingDialog == null) return false;
                 sendBroadcastAsUser(data, UserHandle.CURRENT_OR_SELF);
                 runOnUiThread(() -> mApplyingDialog.updateData(data));
                 return true;
